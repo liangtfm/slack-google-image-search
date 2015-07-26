01ee00e6f74c9233f88e6453cd47accd88918ace
Anthony Liang <liangtfm@gmail.com>
2015-07-26 14:53:20 -0400

    Add image search controller and route

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,21 @@
+require 'json'
+
 class SearchController < ApplicationController
 	def image
+		@base_url = 'https://www.googleapis.com/customsearch/v1'
+		@key = '?key=' + ENV['GOOGLE_API_KEY']
+		@cx = '&cx=' + ENV['GOOGLE_CX']
+		@query = '&q=' + params[:text]
+		@type = '&searchType=image'
 
+		@url = @base_url + @key + @cx + @query + @type
+
+		@response = HTTParty.get(@url)
+		@result = JSON.parse(@response.body)
+		if @result['items']
+			render json: @result['items'].first['link']
+		else
+			render json: 'Nothing found!'
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post 'googleimage' => 'search#image'
+  get 'googleimage' => 'search#image'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
